4f218747dc10feae0e861adbb77d55450525c04d
Infinity2209 <anantanag91@gmail.com>
2024-08-07 21:54:19 +0530

    Initial commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Button (props) {
-    return ( 
+function Button(props) {
+    return (
         <button className={props.buttonClass} onClick={props.onClick}>
-            { props.isIcon ? <React.Fragment><i className={props.isIcon}></i></React.Fragment> : props.textValue }
+            {props.isIcon ? <React.Fragment><i className={props.isIcon}></i></React.Fragment> : props.textValue}
         </button>
-     );
+    );
 }
- 
+
 export default Button;
